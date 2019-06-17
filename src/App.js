31d5e1e97b621b62a7daa291a73ebb9039946d28
VasilisKalaitzis <cs101092@gmail.com>
2019-06-17 03:02:10 +0300

    Trying to fix broken url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import { Provider } from "react-redux";
-import store from "./store";
 import Routes from "./components/Routes/index";
 import "./css/App.css";
 import "./css/containers.css";
@@ -18,11 +16,9 @@ library.add(faChevronLeft, faCalendarAlt, faBook);
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <div className="App color-pallete3">
-          <Routes />
-        </div>
-      </Provider>
+      <div className="App color-pallete3">
+        <Routes />
+      </div>
     );
   }
 }
